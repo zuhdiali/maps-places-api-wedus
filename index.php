fa035e3d241c103bcc5b370bb97205048e106575
zuhdiali <zuhdialihisyam@gmail.com>
2025-07-24 21:38:45 +0700

    update wedus dan logo

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ $dotenv->load();
                         <div class="col-md-12 grid-margin">
                             <div class="row">
                                 <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                                    <h3 class="font-weight-bold">Selamat Datang di WEDUS (Wisata Kabupaten Kudus)</h3>
+                                    <h3 class="font-weight-bold">Selamat Datang di Wisata Kabupaten Kudus</h3>
                                     <!-- <h6 class="font-weight-normal mb-0">
                       Cari informasi tempat wisata di
                       <span class="text-primary">Kudus!</span>
